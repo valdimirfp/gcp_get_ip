b9048dfe27c621b352caa3962043f56edef2e9a6
valdimirfp <46044451+valdimirfp@users.noreply.github.com>
2022-05-24 16:31:54 +0300

    edit

--- gcp_ip_list_v3.sh
+++ gcp_ip_list_v3.sh
@@ -20,7 +20,7 @@ done
 # получаем список внешних адресов
 gcloud projects list --format="get(PROJECT_ID)" | while read pr; do gcloud compute addresses list --project $pr -q 2> /dev/null| awk '/EXTERNAL/ && !/RESERVED/ {print $2 }' ; done >> gcp_ip_A_dns_*
 
-# удалить повторы и стокиначинающиеся с 10
+# удалить повторы и строки начинающиеся с 10
 awk '!x[$0]++' gcp_ip_A_dns_* | sed '/^10/d' >> gcp_ip_A_dns_"$(date +"%d-%m-%Y")"_SORT.txt
 
 echo "END script"
